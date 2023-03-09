b5e49628ae3c26900cd38af41cced0f6cd36cebb
hongyuezhilian <7924503+hongyuezhilian@user.noreply.gitee.com>
2023-03-09 23:22:40 +0800

    Site updated: 2023-03-09 23:22:40

--- js/player.js
+++ js/player.js
@@ -23,39 +23,19 @@ class Musics {
   constructor() {
       this.songs = [{
               id: 1,
-              title: '越权访问-崩坏：星穹铁道 - Hanser',
-              singer: '越权访问-崩坏：星穹铁道 - Hanser',
+              title: '阿衣莫',
+              singer: '阿吉太组合',
               songUrl: './songs/1.mp3',
-              imageUrl: './images/songs/1.jpg'
+              imageUrl: './images/songs/1.png'
           },
           {
               id: 2,
-              title: '妄想Reality（翻自 洛天依） - 炎祾',
-              singer: '妄想Reality（翻自 洛天依） - 炎祾',
+              title: '我不曾忘记',
+              singer: '花玲、张安琪、沐霏',
               songUrl: './songs/2.mp3',
-              imageUrl: './images/songs/2.webp'
+              imageUrl: './images/songs/2.png'
           },
-    {
-              id: 3,
-              title: '人间失宠 - 昔诺',
-              singer: '人间失宠 - 昔诺',
-              songUrl: './songs/3.mp3',
-              imageUrl: './images/songs/3.webp'
-          },
-          {
-              id: 4,
-              title: '崩坏世界的歌姬 (Movie Ver.) - 小林未郁',
-              singer: '崩坏世界的歌姬 (Movie Ver.) - 小林未郁',
-              songUrl: './songs/4.mp3',
-              imageUrl: './images/songs/4.webp'
-          },
-    {
-              id: 5,
-              title: '玛德琳娜电塔 - 花玲',
-              singer: '玛德琳娜电塔 - 花玲',
-              songUrl: './songs/5.mp3',
-              imageUrl: './images/songs/5.webp'
-          },
+    
       ]
   }
   //根据索引获取歌曲的方法
